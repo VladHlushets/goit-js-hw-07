ed9ba81da61da95e1ad96cc59fac8b383906a662
VladHlushets <hlushetsvlad@gmail.com>
2021-04-12 11:54:09 +0300

    add 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,24 @@
-const inputEl = document.getElementById('validation-input');
+const validationInputRef = document.getElementById('validation-input');
+// console.log(validationInputRef);
 
-const fillInput = () => {
-     
-}
+const dataLengthStr = validationInputRef.attributes['data-length'].value;
+const dataLength = parseInt(dataLengthStr);
+// console.log(dataLength);
 
-inputEl.addEventListener('input', fillInput)
+validationInputRef.addEventListener('blur', addRemoveClassOnInput)
+
+function addRemoveClassOnInput() {
+const inputValueLength = validationInputRef.value.length;
+    //   console.log(inputValueLength);
+    if (inputValueLength === dataLength) {
+   changeClass('valid', 'invalid')
+    }
+    else {
+    changeClass('invalid', 'valid')
+    };
+};
+
+const changeClass = (add, remove) => {
+    validationInputRef.classList.add(add);
+    validationInputRef.classList.remove(remove);
+ };
